dc93ce0de1cb858b726136bf34833558914dc61c
Rian20222 <157659618+Rian20222@users.noreply.github.com>
2024-01-31 19:35:06 -0300

    Add files via upload

---
+++ ATIVIDADE AULA 3.py
@@ -0,0 +1,37 @@
+### 1 - Crie um programa para efetuar a leitura de um número inteiro e apresentar o resultado do quadrado deste número.
+
+numero =int(input('Digite um número')) 
+
+print (numero*numero)
+
+
+#### 2 Crie duas variáveis para armazenar seu primeiro nome e sobrenome. Em seguida, concatene-as para formar seu nome completo e exiba o resultado.
+
+Nome = 'seu nome'
+Sobrenome = 'seu sobrenome'
+
+print ('seu nome é,' nome 'seu sobrenome é:,'  Sobrenome)
+
+
+
+### 3 -Peça ao usuário para digitar dois números inteiros e armazene-os em variáveis. Realize a concatenação desses números como strings e exiba o resultado.
+
+Número1 =int(input ('digite um número:'))
+
+Número2 =int(input('digite outro número'))
+
+print (Número1+Número2)
+
+
+### 4 Crie uma variável para armazenar a palavra "Python". Em seguida, adicione um número inteiro ao final da palavra usando a concatenação e exiba o resultado.
+
+a = 'python'
+
+print (a,"5")
+
+5 -#Declare uma variável contendo uma frase. Em seguida, peça ao usuário para digitar uma palavra e concatene essa palavra no final da frase. Exiba o resultado.
+Frase 'Olá mundo'
+
+Número input('digite uma palavra')
+
+print (frase,palavra )
